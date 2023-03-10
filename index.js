2d7d91ee0166f3b234d0f0661f8c800b54f9eec3
komangsuryasedana <komangsuryasedana@mail.com>
2023-03-10 23:24:13 +0800

    tambahan baru

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-console.log("HALLO WORLD!")
+const express = require ("express")
+const { default: next } = require("next")
+const app = express()
+const port = 2001
 
-let color = require ("colors")
+//midleware : menjalankan semua jenis request
+const login = (req,res,next) => {
+            console.log(`${req.method},${req.url}`)
+        next()
+    }
 
-console.log(color.blue('INI WARNA BIRU'))
+app.use(login)
+
+//get semacam routing 
+app.get("/", //patch 
+(req,res)=> {res.send('helo world!') //handler 
+})
+
+app.post("/", //patch 
+(req,res)=> {res.send('helo world!') //handler 
+})
+
+app.get("/product",(req,res)=>{
+    res.json([
+    'apple',
+    'samsung',
+    'xiaomi',
+    'oppo',
+    ])
+})
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`server sedang berjalan di http://localhost:${port}`)
+})
+
